696a5ca9b62160fcef88838e22256edd663f01ea
YU <yulu8798@gmail.com>
2014-04-10 13:29:04 +0800

    test database build and test, ready for large database

--- MainSource/PatternRecognition.py
+++ MainSource/PatternRecognition.py
@@ -16,6 +16,8 @@ from Signature import Signature
 from Tree import Tree
 from Weight import Weight
 
+import random
+
 
 class PatternRecognition:
     
@@ -46,6 +48,19 @@ class PatternRecognition:
         diff = np.fabs(diff)
         return np.sum(diff)
     
+    def RandomSelection(self, input_list, sel):
+        if sel > len(input_list):
+            sel = len(input_list)
+            
+        r_list = []
+        r_set = set()
+        while len(r_list) < sel:
+            rand_n = random.randint(0, len(input_list)-1)
+            if rand_n not in r_set:
+                r_set.add(rand_n)
+                r_list.append(rand_n)
+        return r_list
+    
     '''
     specify the class index and path to the image giving a img directory
     a version num is given, the trained database should access this table by giving the version and number
@@ -54,34 +69,45 @@ class PatternRecognition:
     
     def Build_Database(self, 
                        img_dir, 
-                       num_in_set, #number of image in each set
+                       num_in_set, #number of image in each train set
+                       num_in_test_set, #number of image in each test set
                        num_of_sets, #total number of sets
                        version #version num to store the database
                        ):
         
         if not os.path.isfile(os.path.join(PR.DATABASE_DIR, 'database_'+str(version))):
             print 'generate database'
-            database = []
+            database = [] #train database
+            test_database = [] #test database
             set_index = 0
             for folder_name in os.listdir(img_dir):
+                
                 if set_index >= num_of_sets:
                     break;
-                index = 0;
-                for file_name in os.listdir(os.path.join(img_dir, folder_name)): #later randomize this
-                    if index >= num_in_set:
-                        break
-                    
-                    data_set = [index, set_index, os.path.join(img_dir, folder_name,file_name)]
-                    
-                    index += 1
+                
+                file_list = os.listdir(os.path.join(img_dir, folder_name))
+                
+                sel = self.RandomSelection(range(0, len(file_list)), num_in_set) #random selection from the files in the dir
+                sel_test = self.RandomSelection(range(0, len(file_list)), num_in_test_set)
+                
+                for i in range(0, num_in_set):                  
+                    file_name = file_list[sel[i]]                  
+                    data_set = [i, set_index, os.path.join(img_dir, folder_name, file_name)]
                     
                     database.append(data_set) 
+                    
+                
+                for j in range(0, num_in_test_set):
+                    file_name = file_list[sel_test[j]]
+                    test_data_set = [j, set_index, os.path.join(img_dir, folder_name, file_name)]
+                    
+                    test_database.append(test_data_set)
                     
                     
                 set_index += 1
                 
             #save database
-            db_to_save = [img_dir, num_in_set, num_of_sets, database]
+            db_to_save = [num_in_set, num_of_sets, database, test_database]
             if not os.path.isdir(self.DATABASE_DIR):
                 os.mkdir(self.DATABASE_DIR)
                 
@@ -111,9 +137,9 @@ class PatternRecognition:
                  ):
                     
         #load information from database
-        num_in_set = database[1]
-        num_of_sets = database[2]
-        data_dir = database[3]   
+        num_in_set = database[0]
+        num_of_sets = database[1]
+        data_dir = database[2]   
         total = num_in_set * num_of_sets
 
         updated = force_update # will be turned true if one of the steps has been processed, so that the following step will be focused to processs!
@@ -245,10 +271,7 @@ class PatternRecognition:
                                 depth = 4,
                                 nleaves = 10000):
                 
-        #load information from database
-        num_in_set = database[0]
-        num_of_sets = database[1]       
-        total = num_in_set * num_of_sets
+
         
         updated = self.Train_Database_Sign(database,version, num_of_kpts, force_update, K, depth, nleaves)
         
@@ -275,70 +298,104 @@ class PatternRecognition:
         
         else:
             print 'Weighted Sign Has Already Been Generated'
+            
+            
         
-    def Classifier(self, num_kpts, img_dir, total_test_img, num_in_set, K, depth, cutoff, top=5):
-        classify_result =[]
+        
+    def Classifier(self, 
+                   database,
+                   version,
+                   num_of_kpts,
+                   cutoff,
+                   top = 5,
+                   K = 10,
+                   depth=4):
+        
+        num_in_set = database[0]
+        num_of_sets = database[1]
+        test_database = database[3]  
+        
+        total = num_in_set * num_of_sets
+        num_in_test_set = len(test_database) / num_of_sets 
+        
+        classify_result =np.zeros(num_of_sets)
         #load tree and weight
         wt = Weight(cutoff)
-        wt.load_weights('./Signature/1000/', 'weights')
-        wt.load_weighted_sign('./Signature/1000/', 'weighted_sign')
+        sign_dir = os.path.join(self.SIGN_DIR, 'db_version_' + str(version))
+        wt.load_weights(sign_dir, self.WEIGHT_FILE+str(num_of_kpts)+'_'+str(cutoff))
+        wt.load_weighted_sign(sign_dir, self.WEIGHT_SIGN_FILE+str(num_of_kpts)+'_'+str(cutoff))
     
         #Load tree
+        tree_dir = os.path.join(self.TREE_DIR, 'db_version_' + str(version))
         tr = vl._vlfeat.VlHIKMTree(0, 0)
-        tr.load(self.TREE_DIR+str(num_kpts)+self.TREE_FILE)
-        
+        tr.load(os.path.join(tree_dir, str(num_of_kpts) + self.TREE_FILE))    
 
-        for (k, image_name) in self.database:
+        for k in test_database:
             #randomly get image from the img_dir
-            img = Image.open(os.path.join(folder_name, file_name))
+            img = Image.open(k[2])
             img_data = np.asarray(img, dtype=float)
             
             #generate desc, sign and weighted sign
             kp = Keypoint()
-            kp.load_keypoint(self.KEYPOINT_DIR, self.KEYPOINT_FILE+str(num_kpts))
+            kp.load_keypoint(self.KEYPOINT_DIR, self.KEYPOINT_FILE+str(num_of_kpts))
             desc = Descriptor()
             desc.generate_desc(img_data, kp.kpt)
+            #very important !! convert desc to float type
+            desc_f = np.array(desc.desc, dtype=float)
+            
             sign = Signature()
-            s = sign.generate_sign(tr, desc.desc, K, depth)
+            s = sign.generate_sign(tr,desc_f, K, depth)
             weighted_sign = wt.weight_sign(s)
             
             #vote
-            d=np.empty(self.total_img)
-            for i in range(0, self.total_img):   
+            d=np.empty(total)
+            for i in range(0, total):   
                 d[i] = self.dist(wt.weighted_sign[i,:], weighted_sign)
-        
+            
             perm = np.argsort(d)
             vote_for = np.floor((perm[0:top])/num_in_set)+1                      
-            votes = vl.vl_binsum(np.zeros(self.total_set), np.ones(top), vote_for)
+            votes = vl.vl_binsum(np.zeros(num_of_sets), np.ones(top), vote_for)
             
             #print votes
             best = np.argmax(votes)
             
-            classify_result[k] = best
+            if best == k[1]:
+                classify_result[k[1]] += 1
+                
+            print '=>'+str(k[0])
+        
+        classify_result = classify_result / num_in_test_set
+        
+        print classify_result
+        
             
 if __name__ =='__main__':
     num_of_sets = 15
+    num_of_image_in_test = 8
     num_of_image = 10
     image_folder = './Image/'
-    version = 2
+    version = 3
     
     PR = PatternRecognition()
     
     #generate the training database
-    PR.Build_Database(image_folder, num_of_image, num_of_sets, version)
+    PR.Build_Database(image_folder, num_of_image, num_of_image_in_test, num_of_sets, version)
     
     #load the training database   
     try:
         with open(os.path.join(PR.DATABASE_DIR, 'database_'+ str(version)), 'rb') as rfile:
             database = pickle.load(rfile)
-
     except IOError as ioerr:
         print ioerr
-    
 
-    num_of_kpts = 1000
+
+    num_of_kpts = 2000
     cutoff = 0.01
     #if buid with another database version, indicate a force update!!
-    PR.Build_Weight_Database(database,version,num_of_kpts, cutoff)
+    #PR.Build_Weight_Database(database,version,num_of_kpts, cutoff)
+    
+    #PR.Classifier(database, version, num_of_kpts, cutoff, 1)
+    
+    
     
         
